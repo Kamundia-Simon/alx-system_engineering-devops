72dd5967752dae27f6845fe9ca2d569cc099048f
Kamundia-Simon <simonkamundia8@gmail.com>
2024-07-03 03:06:27 +0300

    puppet install nginx

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -15,11 +15,6 @@ file {'/var/www/html/index.html':
 	content => 'Hello World!'
 }
 
-exec {'redirect_me':
-	command => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
-	provider => 'shell'
-}
-
 service {'nginx':
 	ensure => running,
 	require => Package['nginx']
