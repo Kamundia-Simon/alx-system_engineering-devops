30849310b718cc562f810ea3a921680bf15c49a5
Kamundia-Simon <simonkamundia8@gmail.com>
2024-07-04 00:45:36 +0300

    installed using pip3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,16 @@
 #install flask from pip3.
 
+package { 'python3.8':
+  ensure   => '3.8.10',
+  provider => 'pip3',
+}
+
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'gem',
+  provider => 'pip3',
+}
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
 }
