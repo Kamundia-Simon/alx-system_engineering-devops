511fa8281fc87a31f274c3238689e14a29508516
Kamundia-Simon <simonkamundia8@gmail.com>
2024-07-14 01:55:42 +0300

    Add a custom HTTP header with Puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,36 +1,36 @@
-# Automate adding a custom header with puppet
-
+# New header with puppet
 exec { 'update system':
   command => '/usr/bin/apt-get update',
 }
 
+# Ensure the nginx is installed
 package { 'nginx':
-  ensure => 'installed',
+  ensure  => 'installed',
   require => Exec['update system'],
 }
 
-file_line { 'install':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-enabled/default',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://github.com permanent;',
+# Create the index.html file
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
   require => Package['nginx'],
 }
 
-file {'/var/www/html/index.html':
-  content=> 'Hello World!'
+# Add custom redirect rule to the Nginx configuration
+exec { 'redirect_me':
+  command  => 'sed -i "24i\\       rewrite ^/redirect_me https://github.com permanent;" /etc/nginx/sites-available/default',
+  provider => 'shell',
+  require  => Package['nginx'],
 }
 
-# Add custom HTTP header 
-file_line { 'add_custom_header':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-enabled/default',
-  line   => 'add_header X-Served-By $hostname;',
-  match  => '^\\s*add_header X-Served-By.*$',
-  after  => 'location / {',
-  require => Package['nginx'],
+# Add custom HTTP header to the Nginx 
+exec { 'HTTP header':
+  command  => 'sed -i "25i\\       add_header X-Served-By \\$HOSTNAME;" /etc/nginx/sites-available/default',
+  provider => 'shell',
+  require  => Package['nginx'],
 }
-service {'nginx':
+
+# Ensure the Nginx is running
+service { 'nginx':
   ensure  => running,
   enable  => true,
   require => Package['nginx'],
