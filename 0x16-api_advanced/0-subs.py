d43f3932f1b1aae1cb71e172a34da580eecba089
Kamundia-Simon <simonkamundia8@gmail.com>
2024-08-14 01:49:26 +0300

    How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -15,8 +15,11 @@ def number_of_subscribers(subreddit):
     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "kamundia:0x16.api.advanced:v1.0.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+     try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 404:
+            return 0
+        post = response.json().get("data")
+        return post.get("subscribers", 0)
+    except requests.RequestException:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
