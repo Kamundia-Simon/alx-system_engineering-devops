7f0cb39129decfca286f9655b53cfe9d7bf0da86
Kamundia-Simon <simonkamundia8@gmail.com>
2024-08-14 00:18:30 +0300

    Reddit API number of sucribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,7 +14,7 @@ def number_of_subscribers(subreddit):
         0 if the subreddit is invalid.
     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "0x16.api.advanced/1.0"}
+    headers = {"User-Agent": "kamundia:0x16.api.advanced:v1.0.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         return 0
